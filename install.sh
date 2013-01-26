7a5aced337c4c972ede832ca6f6c0d7cde6458bd
matope <matope.ono@gmail.com>
2013-01-26 22:49:32 +0900

    インストールスクリプト改修

--- install.sh
+++ install.sh
@@ -4,12 +4,16 @@ success () {
   printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
 }
 
-link() {
-  ln -fs $1 $2
-  success "Linked $1 -> $2"
+ask_and_link() {
+  /bin/echo -n "Make link $1 -> $2 ? [y/n]"
+  read ANS
+  if [ $ANS = 'y' -o $ANS = 'yes' ]; then
+    ln -fs $1 $2
+    success "Linked $1 -> $2"
+  fi
 }
 
 DOTFILES_ROOT=`cd $(dirname $0); pwd`
 
-link $DOTFILES_ROOT/.screenrc $HOME/.screenrc
-link $DOTFILES_ROOT/.zshrc $HOME/.zshrc
+ask_and_link $DOTFILES_ROOT/.screenrc $HOME/.screenrc
+ask_and_link $DOTFILES_ROOT/.zshrc $HOME/.zshrc
