2cdf3eca130af7c97b28fcaa02bd6fa7c0655be7
matope <matope.ono@gmail.com>
2015-03-30 16:52:00 +0900

    不要な設定を削除

--- install.sh
+++ install.sh
@@ -1,34 +1,52 @@
 #!/bin/sh
 
-success () {
-  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
-}
-
-
 DOTFILES_ROOT=`cd $(dirname $0); pwd`
-git submodule init
-git submodule foreach 'git pull origin master'
-git submodule update
 
+#======
+# bash
+#======
 ln -siv  $DOTFILES_ROOT/.bash_profile     $HOME/.bash_profile
-ln -siv  $DOTFILES_ROOT/.screenrc         $HOME/.screenrc
+
+#======
+# screen (deactivated)
+#======
+# ln -siv  $DOTFILES_ROOT/.screenrc         $HOME/.screenrc
+
+#======
+# zsh
+#======
 ln -siv  $DOTFILES_ROOT/.zshrc            $HOME/.zshrc
 
+#======
+# tmux
+#======
+ln -siv  $DOTFILES_ROOT/.tmux.conf        $HOME/.tmux.conf
+
+#======
+# git
+#======
 ln -siv  $DOTFILES_ROOT/.gitconfig.common $HOME/.gitconfig.common
-ln -siv  $DOTFILES_ROOT/.gitignore.global $HOME/.gitignore.global
- ln -siv  $DOTFILES_ROOT/.git_template $HOME/.git_template
 if which git > /dev/null 2>&1; then
   git config --global include.path      ~/.gitconfig.common
-  git config --global core.excludesfile ~/.gitignore.global
-  git config --global init.templatedir ~/.git_template
 fi
 
-ln -siv  $DOTFILES_ROOT/.tmux.conf        $HOME/.tmux.conf
-ln -siv  $DOTFILES_ROOT/.tmux.d           $HOME/.tmux.d
+ln -siv  $DOTFILES_ROOT/.gitignore $HOME/.gitignore
+if which git > /dev/null 2>&1; then
+  git config --global core.excludesfile ~/.gitignore
+fi
 
-ln -siv  $DOTFILES_ROOT/.tigrc            $HOME/.tigrc
+# 一旦はずす。
+#ln -siv  $DOTFILES_ROOT/.git_template $HOME/.git_template
+#if which git > /dev/null 2>&1; then
+#  git config --global init.templatedir ~/.git_template
+#fi
 
-sh $DOTFILES_ROOT/Brewfile.sh
-success "Install successfully done."
+
+#======
+# homebrew
+#======
+#sh $DOTFILES_ROOT/Brewfile.sh
+
+echo "Install successfully done."
 
 exit 0
