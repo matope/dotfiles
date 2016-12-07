7658057d64dbeb6660f2c5ca634ea1ec5307cd02
matope <matope.ono@gmail.com>
2016-12-07 19:27:39 +0900

    fix

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 DOTFILES_ROOT=`cd $(dirname $0); pwd`
 
-ln -siv  $DOTFILES_ROOT/.bash_profile     $HOME/bash_profile
-ln -siv  $DOTFILES_ROOT/.zshrc            $HOME/zshrc
-ln -siv  $DOTFILES_ROOT/.tmux.conf        $HOME/tmux.conf
+ln -siv  $DOTFILES_ROOT/bash_profile     $HOME/.bash_profile
+ln -siv  $DOTFILES_ROOT/zshrc            $HOME/.zshrc
+ln -siv  $DOTFILES_ROOT/tmux.conf        $HOME/.tmux.conf
 
 if which git > /dev/null 2>&1; then
   git config --global include.path      $DOTFILES_ROOT/gitconfig
