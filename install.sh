3077f0ba44ecb88ab71d907a50e417c88793ba0d
matope <matope.ono@gmail.com>
2023-05-05 23:13:00 +0900

    git を.config対応

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 set -x
-DOTFILES_ROOT=$(cd $(dirname $0); pwd)
-cd $DOTFILES_ROOT
 
 ln -sv $DOTFILES_ROOT/.bash_profile   $HOME/.bash_profile
 ln -sv $DOTFILES_ROOT/.bashrc         $HOME/.bashrc
@@ -10,10 +8,4 @@ ln -sv $DOTFILES_ROOT/.tmux.conf      $HOME/.tmux.conf
 ln -sv $DOTFILES_ROOT/.zprofile       $HOME/.zprofile
 ln -sv $DOTFILES_ROOT/.zshrc          $HOME/.zshrc
 ln -sv $DOTFILES_ROOT/.zsh            $HOME/
-
-if which git >/dev/null 2>&1; then
-  git config --global include.path      $DOTFILES_ROOT/gitconfig
-  git config --global core.excludesfile $DOTFILES_ROOT/gitignore_global
-fi
-
-echo "Install successfully done."
+ln -sv $DOTFILES_ROOT/git             $HOME/.config/
